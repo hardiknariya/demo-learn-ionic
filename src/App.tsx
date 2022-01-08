863275e165675de01e871333a23ffa81b8be00a4
HardikNariya <hardiknariya308@gmail.com>
2022-01-08 11:35:02 +0530

    learning ionic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import {
   IonApp,
-  IonCard,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
+  IonButton,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
   setupIonicReact,
 } from "@ionic/react";
+import { calculator, star, refreshCircle } from "ionicons/icons";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -30,44 +38,96 @@ import "@ionic/react/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
+import { useState } from "react";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+const App: React.FC = () => {
+  const [width, setWidth] = useState<number>();
+  const [height, setHeight] = useState<number>();
+
+  return (
+    <IonApp>
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle>Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <IonCard
-          style={{
-            textAlign: "center",
-            position: "absolute",
-            left: 0,
-            right: 0,
-            top: "50%",
-            transform: "translateY(-50%)",
-          }}
-        >
-          <IonCardHeader>
-            <IonCardTitle className="ion-text-center">Ready to create an app?</IonCardTitle>
-            <IonCardSubtitle className="ion-text-center ion-margin-top">
-              {/* https://ionicframework.com/docs/components */}
-            Start with Ionic
-            </IonCardSubtitle>
-          </IonCardHeader>
-        </IonCard>
-      </IonContent>
-    </IonPage>
-  </IonApp>
-);
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Home</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonGrid fixed={false}>
+            <IonRow className="ion-margin-bottom">
+              <IonCol sizeXs="12" sizeSm="6" sizeLg="6" sizeMd="6" sizeXl="6">
+                <IonItem>
+                  <IonLabel position="floating">Width</IonLabel>
+                  <IonInput
+                    value={width}
+                    type="number"
+                    onIonChange={(e) => setWidth(parseInt(e.detail!.value!))}
+                  />
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-margin-bottom">
+              <IonCol sizeXs="12" sizeSm="12" sizeLg="6" sizeMd="6" sizeXl="6">
+                <IonItem>
+                  <IonLabel position="floating">Height</IonLabel>
+                  <IonInput
+                    value={height}
+                    type="number"
+                    onIonChange={(e) => setHeight(parseInt(e.detail!.value!))}
+                  />
+                </IonItem>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-margin-bottom">
+              <IonCol sizeXs="6" sizeSm="6" sizeLg="3" sizeMd="3" sizeXl="3">
+                <IonButton color="primary" expand="block">
+                  <IonIcon slot="start" icon={calculator} />
+                  Calculator
+                </IonButton>
+              </IonCol>
+              <IonCol sizeXs="6" sizeSm="6" sizeLg="3" sizeMd="3" sizeXl="3">
+                <IonButton color="secondary" expand="block">
+                  Reset
+                  <IonIcon slot="start" icon={refreshCircle} />
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+
+          <IonItemSliding>
+            <IonItemOptions side="start">
+              <IonItemOption onClick={() => console.log("favorite clicked")}>
+                Favorite
+              </IonItemOption>
+              <IonItemOption
+                color="danger"
+                onClick={() => console.log("share clicked")}
+              >
+                Share
+              </IonItemOption>
+            </IonItemOptions>
+
+            <IonItem>
+              <IonLabel>Item Options</IonLabel>
+            </IonItem>
+
+            <IonItemOptions side="end">
+              <IonItemOption onClick={() => console.log("unread clicked")}>
+                Unread
+              </IonItemOption>
+            </IonItemOptions>
+          </IonItemSliding>
+        </IonContent>
+      </IonPage>
+    </IonApp>
+  );
+};
 
 export default App;
---
+++ src/README.md
@@ -0,0 +1,6 @@
+xs < 540px
+sm > 576px
+md > 768px 
+lg > 992px 
+xl > 1200px
+
